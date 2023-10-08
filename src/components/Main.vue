51cd50959a2202127253272c312dd69d73b6a907
danielwalke <daniel.walke@ovgu.de>
2023-10-08 18:58:34 +0200

    removed dist

---
+++ src/components/Main.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>This will be my personal portofolio page. For now this is just some hobby to keep my vue & tailwind skills sharp. Hopefully you will see soe more beautiful stuff here soon.</div>
+  <div>Current date: 08.10.23 - Project initialization</div>
+</template>
+
+<script>
+export default {
+  name: "Main"
+}
+</script>
+
+<style scoped>
+
+</style>
