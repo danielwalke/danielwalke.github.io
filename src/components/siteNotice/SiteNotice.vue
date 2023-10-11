18ff4e9f8e09cea0c76787fe27a6963f47494ed4
danielwalke <daniel.walke@ovgu.de>
2023-10-11 18:58:38 +0200

    first site notice prototype

--- src/components/siteNotice/SiteNotice.vue
+++ src/components/siteNotice/SiteNotice.vue
@@ -1,6 +1,29 @@
 <template>
-<div>
-  Site Notice
+<div class="w-full flex flex-col items-center">
+  <h2 class="text-xl">Impressum</h2>
+
+  <p>Angaben gemäß § 5 TMG:</p>
+
+  <p>Daniel Walke<br>
+  Universitätsplatz 2<br>
+  39106 Magdeburg<br>
+  </p>
+
+  <p>E-Mail: daniel.walke@ovgu.de</p>
+
+  Verantwortlich für den Inhalt nach § 55 Abs. 2 RStV:
+
+  <p>Daniel Walke<br>
+    Universitätsplatz 2<br>
+    39106 Magdeburg<br>
+  </p>
+
+  <h3>Haftungsausschluss (Disclaimer):</h3>
+
+  <p>Haftung für Inhalte</p>
+
+  <p>Die Inhalte unserer Seiten wurden mit größter Sorgfalt erstellt. Für die Richtigkeit, Vollständigkeit und Aktualität der Inhalte können wir jedoch keine Gewähr übernehmen. Als Diensteanbieter sind wir gemäß § 7 Abs.1 TMG für eigene Inhalte auf diesen Seiten nach den allgemeinen Gesetzen verantwortlich.
+  </p>
 </div>
 </template>
 
