5f15972e61de6f714de58304be1dc89bd6569ac8
danielwalke <daniel.walke@ovgu.de>
2023-10-29 18:12:35 +0100

    prototype ii

--- src/components/blog/Intro.vue
+++ src/components/blog/Intro.vue
@@ -5,88 +5,233 @@
       <div class="flex justify-center">
         <img class="bg-gray-600 w-2/4 mb-2 rounded-md" src="../../assets/profile_img.jpeg" alt="Bild"/>
       </div>
-      <h1 class="font-bold text-2xl text-center"> My name is Daniel. </h1>
+      <h1 class="font-bold text-2xl text-center"> My name is Daniel Walke. </h1>
       <p class="text-center text-lg font-bold p-4">
-        I am PhD student at the Otto-von-Guericke University. My topic is optimizing graph databases and graph learning for
-        large clinical and biological datasets. Therefore, I already wrote a literature review with several amazing co-authors.
-        Furthermore, I applied several graph learning algorithms like Graph Neural Networks
+        I am PhD student at the Otto-von-Guericke University. The topic for my PhD is optimizing graph databases and graph learning for
+        large clinical and biological datasets. Therefore, I applied several graph learning algorithms like Graph Neural Networks
         on diverse datasets and aim to improve the predictive power of machine learning using graph structures.
       </p>
     </div>
 
+    <h4 class="subsection-header">Professional Career.</h4>
 
 
-    <div class="flex gap-2">
-      <div class="bg-gray-100 rounded-md p-4 font-bold mb-4">
-        <h4 class="text-lg">Professional Career</h4>
-        <ul class="list-disc p-4">
-          <li>Research fellow (full-time) at the Otto-von-Guericke University: January 2023 till now</li>
-          <li>Research fellow (part-time) at the Otto-von-Guericke University & Software developer (part-time) at the METOP GmbH: December 2021 - January 2023</li>
-          <li>Software developer (full-time) at the METOP GmbH: September 2021 - December 2021</li>
-          <li>Software developer (Internship) at the METOP GmbH: August 2021 - September 2021</li>
-          <li>Research Assistant (HiWi part-time) at the Otto-von-Guericke University: May 2021 - August 2021</li>
-          <li>Research Assistant (HiWi part-time) at the Otto-von-Guericke University: August 2020 - January 2021</li>
-          <li>Tutor (HiWi part-time) at the Otto-von-Guericke University: October 2019 - January 2020</li>
+    <div class="row">
+      <div class="inner-box">
+        <h4 class="text-2xl text-center font-bold p-4 mb-2">Research fellow (full-time)</h4>
+        <ul class="list-none">
+          <li>Where?<br/> <div class="text-right">Otto-von-Guericke University</div></li>
+          <li>When?<br/> <div class="text-right">Since January 2023</div></li>
+          <li>What?<br/> <div class="text-right">Applying graph learning learning algorithms on sepsis complete blood count data (pandas, numpy, scikit-learn,
+            PyTorch, PyG)</div></li>
         </ul>
       </div>
 
-
-      <div class="bg-gray-100 rounded-md p-4 font-bold mb-4">
-        <h4 class="text-lg">Publications</h4>
-        <ul class="list-disc p-4">
-          <li>
-            <h4>The importance of graph databases and graph learning for clinical applications</h4>
-            <p>Daniel Walke, Daniel Micheel, Kay Schallert, Thilo Muth, Gunter Saake, David Broneske, Robert Heyer</p>
-          </li>
+      <div class="inner-box">
+        <h4 class="text-2xl text-center font-bold p-4 mb-2">Research fellow (part-time)</h4>
+        <ul class="list-none">
+          <li>Where?<br/> <div class="text-right">Otto-von-Guericke University</div></li>
+          <li>When?<br/> <div class="text-right">December 2021 - December 2022</div></li>
+          <li>What?<br/> <div class="text-right">Literature survey about graph databases and graph learning</div></li>
         </ul>
       </div>
     </div>
 
 
+    <div class="row">
 
-    <div class="flex gap-2">
-      <div class="bg-gray-100 rounded-md p-4 font-bold mb-4">
-        <h4 class="text-lg">Education</h4>
-        <ul class="list-disc p-4">
-          <li>Master of Science in Biosystems Engineering - final grade 1.3 ('very good')</li>
-          <li>Bachelor of Science in Biosystems Engineering - final grade 1.6 ('good')</li>
-          <li>Abitur - final grade 2.0 ('good')</li>
+      <div class="inner-box lg:w-1/2">
+        <h4 class="text-2xl text-center font-bold p-4 mb-2">Software developer (part-time)</h4>
+        <ul class="list-none">
+          <li>Where?<br/> <div class="text-right">METOP GmbH</div></li>
+          <li>When?<br/> <div class="text-right">December 2021 - December 2022</div></li>
+          <li>What?<br/> <div class="text-right">Web development (Vue, JavaScript, TypeScipt, Parse)</div></li>
         </ul>
       </div>
-      <div class="bg-gray-100 rounded-md p-4 font-bold mb-4">
-        <h4 class="text-lg">Skills</h4>
-        <ul class="list-disc p-4">
-          <li>Web development (HTML, CSS, JavaScript, TypeScript, React, Vue)</li>
-          <li>Backend-technologies (Java, Python, Parse as BaaS)</li>
-          <li>Machine learning and graph learning (pandas, numpy, scikit-learn, PyTorch, PyTorchGeometric)</li>
+
+      <div class="inner-box lg:w-1/2">
+        <h4 class="text-2xl text-center font-bold p-4 mb-2">Software developer (full-time)</h4>
+        <ul class="list-none">
+          <li>Where?<br/> <div class="text-right">METOP GmbH</div></li>
+          <li>When?<br/> <div class="text-right">August 2021 - November 2021</div></li>
+          <li>What?<br/> <div class="text-right">Web development (Vue, JavaScript, Parse)</div></li>
         </ul>
       </div>
     </div>
 
-    <div class="flex gap-2">
-      <div class="bg-gray-100 rounded-md p-4 font-bold mb-4">
-        <h4 class="text-lg">Interests</h4>
-        <ul class="list-disc p-4">
-          <li>Coding</li>
-          <li>Gym</li>
-          <li>Listening to Podcasts (HubermanLab, Lex Friedman)</li>
+    <div class="row">
+
+      <div class="inner-box lg:w-1/3">
+        <h4 class="text-2xl text-center font-bold p-4 mb-2">Research Assistant (part-time)</h4>
+        <ul class="list-none">
+          <li>Where?<br/> <div class="text-right">Otto-von-Guericke University</div></li>
+          <li>When?<br/> <div class="text-right">May 2021 - August 2021</div></li>
+          <li>What?<br/> <div class="text-right">Paper writing & Web development (ReactJS) with Java Backend</div></li>
         </ul>
       </div>
 
-      <div class="bg-gray-100 rounded-md p-4 font-bold mb-4">
-        <h4 class="text-lg">Contact</h4>
-        <ul class="list-disc p-4">
-          <li>Private: daniel-walke@t-online.de</li>
-          <li>Academic: danie.walke@ovgu.de</li>
+      <div class="inner-box lg:w-1/3">
+        <h4 class="text-2xl text-center font-bold p-4 mb-2">Research Assistant (part-time)</h4>
+        <ul class="list-none">
+          <li>Where?<br/> <div class="text-right">Otto-von-Guericke University</div></li>
+          <li>When?<br/> <div class="text-right">August 2020 - January 2021</div></li>
+          <li>What?<br/> <div class="text-right">Web development (ReactJS) with Java Backend</div></li>
+        </ul>
+      </div>
+
+      <div class="inner-box lg:w-1/3">
+        <h4 class="text-2xl text-center font-bold p-4 mb-2">Tutor (part-time)</h4>
+        <ul class="list-none">
+          <li>Where?<br/> <div class="text-right">Otto-von-Guericke University</div></li>
+          <li>When?<br/> <div class="text-right">October 2019 - January 2020</div></li>
+          <li>What?<br/> <div class="text-right">Correction of MATLAB exercises for simulating biotechnological processes</div></li>
         </ul>
       </div>
 
     </div>
+
+    <h4 class="subsection-header">Publications.</h4>
+
+    <div class="row">
+      <div class="inner-box lg:w-1/3">
+        <h4 class="text-2xl"><a href="https://academic.oup.com/database/article/doi/10.1093/database/baad045/7222237" target="_blank">The importance of graph databases and graph learning for clinical applications</a></h4>
+        <p>Daniel Walke*, Daniel Micheel*, Kay Schallert, Thilo Muth, Gunter Saake, David Broneske, Robert Heyer</p>
+        <ul class="list-none p-4">
+          <li>Journal <br/><div class="text-right">DATABASE The Journal of Biological Databses and Curation</div></li>
+        </ul>
+      </div>
+
+      <div class="inner-box lg:w-1/3">
+        <h4 class="text-2xl"><a href="https://pure.mpg.de/rest/items/item_3348333/component/file_3348946/content" target="_blank">MPA_Pathway_Tool: User-Friendly, Automatic Assignment of
+          Microbial Community Data on Metabolic Pathways</a></h4>
+        <p>Daniel Walke*, Kay Schallert, Prasanna Ramesh, Dirk Benndorf, Emanuel Lange, Udo Reichl, Robert Heyer</p>
+        <ul class="list-none p-4">
+          <li>Journal <br/><div class="text-right">International Journal of Molecular Sciences</div></li>
+        </ul>
+      </div>
+    </div>
+
+    <h4 class="subsection-header">Education.</h4>
+
+    <div class="row">
+
+      <div class="inner-box lg:w-1/3">
+        <h4 class="text-2xl">M. Sc. Biosystems engineering</h4>
+        <ul class="list-disc p-4">
+          <li>Final grade: 1.3 ('very good')</li>
+          <li>Master thesis: MPA_Pathway_Tool: User-friendly, automatic assignment of microbial community data on metabolic pathways (grade: 1.0)</li>
+          <li>Otto-von-Guericke University</li>
+        </ul>
+      </div>
+
+      <div class="inner-box lg:w-1/3">
+        <h4 class="text-2xl">B. Sc. Biosystems engineering</h4>
+        <ul class="list-disc p-4">
+          <li>Final grade: 1.6 ('good')</li>
+          <li>Bachelor thesis: Establishment of a new primer set for analyzing the gene expression in <div class="italic">Physarum polychephalum</div> (grade: 1.2)</li>
+          <li>Otto-von-Guericke University</li>
+        </ul>
+      </div>
+
+      <div class="inner-box lg:w-1/3">
+        <h4 class="text-2xl">Abitur</h4>
+        <ul class="list-disc p-4">
+          <li>Final grade: 2.0 ('good')</li>
+          <li>Advanced Courses: Mathematics and Biology</li>
+          <li>Gymnasium Carolinum Bernburg</li>
+        </ul>
+      </div>
+    </div>
+
+    <h4 class="subsection-header">Skills.</h4>
+
+    <div class="row">
+
+      <div class="inner-box lg:w-1/3">
+        <h4 class="text-2xl">Web development</h4>
+        <ul class="list-disc p-4">
+          <li>HTML, CSS, JavaScript</li>
+          <li>ReactJS</li>
+          <li>Vue (with JS & TS)</li>
+          <li>Used backends (Java, Python, Parse as BaaS)</li>
+        </ul>
+      </div>
+
+      <div class="inner-box lg:w-1/3">
+        <h4 class="text-2xl">Machine learning & graph learning</h4>
+        <ul class="list-disc p-4">
+          <li>Pandas</li>
+          <li>Numpy</li>
+          <li>Scikit-learn</li>
+          <li>PyTorch</li>
+          <li>PyTorch Geometric</li>
+        </ul>
+      </div>
+
+      <div class="inner-box lg:w-1/3">
+        <h4 class="text-2xl">Modeling biotechnological processes</h4>
+        <ul class="list-disc p-4">
+          <li>MATLAB</li>
+        </ul>
+      </div>
+
+    </div>
+
+
+    <h4 class="subsection-header">Interests.</h4>
+
+    <div class="row">
+
+      <div class="inner-box lg:w-1/3">
+        <h4 class="text-2xl">Coding</h4>
+        <ul class="list-disc p-4">
+          <li>Like doing some coding challenges on platforms like <a class="underline" target="_blank" href="https://leetcode.com/dwalke/">LeetCode</a></li>
+          <li>I also enjoy making some small coding projects or testing out new stuff.</li>
+        </ul>
+      </div>
+
+      <div class="inner-box lg:w-1/3">
+        <h4 class="text-2xl">Sports & Nutrients</h4>
+        <ul class="list-disc p-4">
+          <li>Doing weight training with Push-Pull-Leg Split</li>
+          <li>Enjoy cooking and try to achieve optimal nutritional supply.</li>
+        </ul>
+      </div>
+
+      <div class="inner-box lg:w-1/3">
+        <h4 class="text-2xl">Listening to Podcasts (Recommendations)</h4>
+        <ul class="list-disc p-4">
+          <li><a href="https://www.hubermanlab.com/podcast" target="_blank">Huberman Lab Podcast</a></li>
+          <li><a href="https://lexfridman.com/podcast/" target="_blank">Lex Fridman Podcast</a></li>
+        </ul>
+      </div>
+
+    </div>
+
+    <h4 class="subsection-header">Contact.</h4>
+
+    <div class="row">
+
+      <div class="inner-box lg:w-1/2">
+        <h4 class="text-2xl">Academic Purposes (Cooperation, Questions, Talks)</h4>
+        <ul class="list-disc p-4">
+          <li>daniel.walke@ovgu.de</li>
+        </ul>
+      </div>
+
+      <div class="inner-box lg:w-1/2">
+        <h4 class="text-2xl">Private Purposes (Questions regarding this page & Job requests)</h4>
+        <ul class="list-disc p-4">
+          <li>daniel-walke@t-online.de</li>
+        </ul>
+      </div>
+    </div>
+
 
 
     <div class="text-sm">Additional Notes: This will be my personal portofolio page. For now this is just some hobby to keep my vue & tailwind skills sharp. Hopefully you will see soe more beautiful stuff here soon.</div>
     <div>Current date: 08.10.23 - Project initialization</div>
-  </div>
+</div>
 </div>
 </template>
 
@@ -97,5 +242,26 @@ export default {
 </script>
 
 <style scoped>
+.inner-box{
+  @apply bg-gray-100 rounded-md p-2 lg:p-6 font-bold mb-4 w-full
+}
 
+.row{
+  @apply flex gap-2 w-full flex-col lg:flex-row
+}
+
+.subsection-header{
+  @apply text-4xl text-center font-bold p-4 mb-4 mt-4
+}
+ul{
+  @apply lg:p-4 p-2
+}
+
+li{
+  @apply mb-2
+}
+
+a{
+  @apply underline mb-2
+}
 </style>
